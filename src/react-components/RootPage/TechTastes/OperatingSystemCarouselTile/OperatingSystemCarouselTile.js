3fd61b7d8c1555d48815d459afbbc53ffcce7651
Zach Huxford <zachbwh@gmail.com>
2019-12-30 08:19:24 +1300

    changed the tech tastes view into a viewport pagination view
    added an OSes section to tech tastes with OS svgs in a carousel view

---
+++ src/react-components/RootPage/TechTastes/OperatingSystemCarouselTile/OperatingSystemCarouselTile.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './OperatingSystemCarouselTile.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class OperatingSystemCarouselTile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            osName: props.osName,
+            osIconClassName: props.osName.toLowerCase().replace(/ /g, "-"),
+            osIconFilePath: "/assets/tech-tastes/" + (props.osName.toLowerCase().replace(/ /g, "-")) + ".svg",
+            osIconAltText: props.osName + " Logo",
+            usageIcons: props.usageIcons
+        };
+    }
+
+    render() {
+        var usageIcons;
+        if (this.state.usageIcons.length > 0) {
+            usageIcons = this.state.usageIcons.map((usageIcon, index) => (
+                <p key={index} ><FontAwesomeIcon icon={usageIcon.icon} /> {usageIcon.description}</p>
+            ));
+        } else {
+            usageIcons = <p></p>
+        }
+        return (
+            <div className="operating-system-carousel-tile">
+                <div className="os-name">
+                    <img className={this.state.osIconClassName + " tech-svg"} src={this.state.osIconFilePath} alt={this.state.osIconAltText}></img>
+                    {this.state.osName}
+                </div>
+                <div className="os-usage">
+                    {usageIcons}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default OperatingSystemCarouselTile;
--- src/react-components/RootPage/TechTastes/TechTastes.js
+++ src/react-components/RootPage/TechTastes/TechTastes.js
@@ -1,21 +1,145 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './TechTastes.css';
 
-class TechTastes extends Component {
-  render() {
-    return (
-        <div className="tech-tastes">
-            <div className="body">
-                <div>
-                    <h1>My Tech Tastes<span className="highlight-full-stop">.</span></h1>
-                    <p>
-                        A graphical list of all the technologies I prefer to use and/or am familiar with.
-                    </p>
+import ViewportPaginationView from '../ViewportPaginationView/ViewportPaginationView';
+import '../ViewportPaginationView/ViewportPaginationView.css';
+import Carousel from '../../Carousel/Carousel';
+import OperatingSystemCarouselTile from './OperatingSystemCarouselTile/OperatingSystemCarouselTile';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faBriefcase, faServer, faMobileAlt, faLaptopCode, faGamepad } from '@fortawesome/free-solid-svg-icons';
+
+
+import IndexIndicator from '../ViewportPaginationView/IndexIndicator/IndexIndicator';
+
+class TechTastes extends ViewportPaginationView {
+
+    hashes = ["", "operating-systems", "programming-languages", "dev-ops"]
+
+    operatingSystemsList = [
+        {
+            osName: "Arch Linux",
+            usageIcons: [
+                {
+                    icon: faHome,
+                    description: "OS for Personal Use"
+                },
+                {
+                    icon: faGamepad,
+                    description: "OS used for Gaming"
+                },
+                {
+                    icon: faLaptopCode,
+                    description: "OS used on my Laptop"
+                }
+            ]
+        },
+        {
+            osName: "Windows",
+            usageIcons: [
+                {
+                    icon: faBriefcase,
+                    description: "OS for Work Use"
+                },
+                {
+                    icon: faGamepad,
+                    description: "OS used for Gaming"
+                },
+                {
+                    icon: faLaptopCode,
+                    description: "OS used on my Laptop"
+                }
+            ]
+        },
+        {
+            osName: "Ubuntu",
+            usageIcons: [
+                {
+                    icon: faServer,
+                    description: "OS for Server Use"
+                },
+                {
+                    icon: faLaptopCode,
+                    description: "OS used on my Laptop"
+                }
+            ]
+        },
+        {
+            osName: "Android",
+            usageIcons: [
+                {
+                    icon: faHome,
+                    description: "OS for Personal Use"
+                },
+                {
+                    icon: faMobileAlt,
+                    description: "OS used on my Phone"
+                }
+            ]
+        },
+        {
+            osName: "Raspbian",
+            usageIcons: [
+                {
+                    icon: faServer,
+                    description: "OS for Raspberry Pi Server Use"
+                }
+            ]
+        },
+        {
+            osName: "EC2 AMI",
+            usageIcons: [
+                {
+                    icon: faServer,
+                    description: "OS for Server Use"
+                }
+            ]
+        }
+    ];
+
+
+    render() {
+        var operatingSystemCarouselTiles = this.operatingSystemsList.map(operatingSystem => (
+            <OperatingSystemCarouselTile osName={operatingSystem.osName} usageIcons={operatingSystem.usageIcons} />
+        ))
+        return (
+            <div className="viewport-pagination-view tech-tastes" onWheel={this.handleScroll.bind(this)} onTouchMove={this.handleTouchMove.bind(this)} onTouchStart={this.registerTouchStart.bind(this)}>
+                <IndexIndicator hashes={this.hashes} activeIndex={this.state.currentIndex} setIndex={this.setIndex.bind(this)}></IndexIndicator>
+                <div className="viewport-container" style={{transform: "translateY(-" + this.state.currentIndex * this.state.windowHeight + "px)"}}>
+                    <div className="viewport" style={{height: this.state.windowHeight + "px"}}>
+                        <div>
+                            <h1>My Tech Tastes<span className="highlight-full-stop">.</span></h1>
+                            <div className="scroll-down">
+                                <p>SCROLL DOWN</p>
+                                <FontAwesomeIcon icon={faChevronDown} />
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="viewport" style={{height: this.state.windowHeight + "px"}}>
+                        <div>
+                            <h1>OSes</h1>
+                            <Carousel carouselItems={operatingSystemCarouselTiles} />
+                        </div>
+                    </div>
+
+                    <div className="viewport" style={{height: this.state.windowHeight + "px"}}>
+                        <div>
+                            <h1>Programming Languages</h1>
+                            <div className="os-grid"></div>
+                        </div>
+                    </div>
+
+                    <div className="viewport" style={{height: this.state.windowHeight + "px"}}>
+                        <div>
+                            <h1>Dev Ops</h1>
+                            <div className="os-grid"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default TechTastes;
