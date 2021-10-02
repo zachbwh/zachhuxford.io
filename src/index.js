c1ba4e75affc01df1ca75e8d2dd169d3991cb849
Zach Huxford <zachbwh@gmail.com>
2021-10-02 14:13:52 +1300

    removed google analytics

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-107350487-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 ReactDOM.render(<App />, document.getElementById('root'));
